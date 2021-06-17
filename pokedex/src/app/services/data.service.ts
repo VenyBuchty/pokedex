7d3eae8cf6230cc81b557285e09b0063e73aeffc
Vendula Buchtova <ven.buchtova@gmail.com>
2021-06-17 09:39:22 +0200

    data pokemon fetched in details

--- pokedex/src/app/services/data.service.ts
+++ pokedex/src/app/services/data.service.ts
@@ -11,7 +11,7 @@ export class DataService {
   ) { }
 
   getPokemons() {
-    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=50`);
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=100`);
   }
 
   getMoreData(name: string) {
